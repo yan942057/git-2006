86e1736f850dd6463a014bdef1e6dcb3b57b183e
zhangsan <123@qq.com>
2020-12-29 21:49:03 +0800

    项项目基础环境的创建[C

---
+++ src/test/java/com/baizhi/git2006/Git2006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.git2006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git2006ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
